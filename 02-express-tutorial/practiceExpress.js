5009d9df3a20f1bc8d0aade4e1cd5dfb4bbbc8f9
miguel <74852114+mickeygcodes@users.noreply.github.com>
2023-01-04 21:47:35 -0800

    Answers for lesson 4

--- 02-express-tutorial/practiceExpress.js
+++ 02-express-tutorial/practiceExpress.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const consoleLog = require("./practice-middleware");
 
 const app = express();
 const PORT = 3000;
+
+app.use(consoleLog);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/new-public/index.html");
